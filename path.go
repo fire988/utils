2cf5c84a938e62e6b1ee1df89d0a13ee3d1bae1c
fyn <fyn@icardpack.com>
2017-06-12 10:56:26 +0800

    add Dir function

---
+++ path.go
@@ -0,0 +1,23 @@
+package utils
+
+import (
+	"os"
+	"strings"
+)
+
+//Dir return the dir part of a path.
+//NOTE: not a clean path!
+func Dir(path string) string {
+	pos := strings.LastIndexByte(path, os.PathSeparator)
+	if pos == -1 {
+		if os.PathSeparator != '/' {
+			pos = strings.LastIndexByte(path, '/')
+			if pos == -1 {
+				return "."
+			}
+			return path[:pos+1]
+		}
+	}
+
+	return path[:pos+1]
+}
