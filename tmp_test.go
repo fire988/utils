9aa89fcd00e88dcaa5e79c7357f436e16deb7aba
jamie <jamie@61.190.149.122:29418>
2017-08-13 09:03:37 +0800

    add test

--- tmp_test.go
+++ tmp_test.go
@@ -13,3 +13,25 @@ func TestGenRandomPass(t *testing.T) {
 		fmt.Println("pass:", s)
 	}
 }
+
+func TestDigiCode(t *testing.T) {
+	s := ""
+	s = GenRandomDigiCode(6)
+	if len(s) != 6 {
+		t.Error("error.")
+	} else {
+		fmt.Println(s)
+	}
+	s = GenRandomDigiCode(8)
+	if len(s) != 8 {
+		t.Error("error.")
+	} else {
+		fmt.Println(s)
+	}
+	s = GenRandomDigiCode(16)
+	if len(s) != 16 {
+		t.Error("error.")
+	} else {
+		fmt.Println(s)
+	}
+}
